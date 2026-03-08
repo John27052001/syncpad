38e2c4450ce63e544b572b6bf020731dd471ebee
MeghaJohn <008458288@coyote.csusb.edu>
2026-03-07 21:42:37 -0800

    Clean history: remove node_modules

--- backend/dist/documents/dto/update-document.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateDocumentDto = void 0;
-class UpdateDocumentDto {
-}
-exports.UpdateDocumentDto = UpdateDocumentDto;
-//# sourceMappingURL=update-document.dto.js.map
--- frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"2OQO8IrtSXMVEvdj17cEI2NGeEoCCL5u69JwknITxjU=\"\n}"
