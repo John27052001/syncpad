edda9b2ba986b48700d081d8b4b67aede6118df8
MeghaJohn <008458288@coyote.csusb.edu>
2026-05-10 19:11:02 -0700

    Fix CORS issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,16 +4,21 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://syncpad-alpha.vercel.app',
+      'http://localhost:3000',
+    ],
+    credentials: true,
+  });
+
   app.useWebSocketAdapter(new IoAdapter(app));
-app.enableCors({
-  origin: [
-    'https://syncpad-7p3ff8u98-meghajohnbabu-6318s-projects.vercel.app',
-    'http://localhost:3000'
-  ],
-  credentials: true,
-});
+
   const port = process.env.PORT || 4000;
   await app.listen(port);
-  console.log(`SyncPad backend running on http://localhost:${port}`);
+
+  console.log(`SyncPad backend running on port ${port}`);
 }
+
 bootstrap();
